d5b96b622631b0e0cb392b964bfcb53714b964e8
minoritydev <debarghyagsaha@gmail.com>
2024-06-02 07:27:19 +0530

    init

---
+++ app/api/add/route.js
@@ -0,0 +1,23 @@
+export const dynamic = "force-dynamic";
+const token = process.env.DB_TOKEN;
+export async function GET(request) {
+  if (!request.nextUrl.searchParams.get("todo")) {
+    return new Response("Todo is blank :(", {
+      status: 200,
+    });
+  }
+  let todo = encodeURI(request.nextUrl.searchParams.get("todo"));
+  const url = "https://decent-chigger-32382.upstash.io/lpush/todo/" + todo + "?_token=" + token;
+
+  //make a get request using url
+
+  return fetch(url) 
+    .then((r) => r.json())
+    .then((data) => {
+      let result = JSON.stringify(data.result);
+      return new Response(result, {
+        status: 200,
+      });
+    });
+  }
+
---
+++ app/api/list/route.js
@@ -0,0 +1,18 @@
+export const dynamic = "force-dynamic";
+const token = process.env.DB_TOKEN;
+const url = "https://decent-chigger-32382.upstash.io/lrange/todo/0/100?_token=" + token;
+export async function GET(request) {
+ //fetch data from upstash
+
+  return fetch(url,{
+    next: { revalidate: 60 }, // Revalidate every 60 seconds
+  }) 
+    .then((r) => r.json())
+    .then((data) => {
+      let result = JSON.stringify(data.result);
+      return new Response(result, {
+        status: 200,
+      });
+    });
+
+}
